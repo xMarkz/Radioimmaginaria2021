92e12f49f748116a7a7d3e5f3a81698867a3d83c
Mark <desidmarco@gmail.com>
2021-07-28 10:05:35 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,88 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Radioimmaginaria - La radio degli adolescenti e il network in Europa, fatto, diretto e condotto da noi che abbiamo da 11 a 17 anni.">
+    <meta name="author" content="Dante, Mark, and the cast of Radio Immaginaria">
+
+		<!-- Favicon -->
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>Radio Immaginaria - La radio degli adolescenti</title>
+
+    <link rel="canonical" href="https://radioimmaginaria.it/" />
+
+		<!-- Google fonts -->
+		<link href="https://fonts.googleapis.com/css2?family=Spartan" rel="stylesheet" type="text/css" />
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+
+		<!-- Masthead -->
+		<header class="masthead">
+			<div class="container">
+				<div class="text-uppercase">Radioimmaginaria</div>
+				<div>La radio degli adolescenti</div>
+				<div class="align-items-center">
+                    <p>Dal 2012 Radioimmaginaria è il network in Europa, fatto, diretto e condotto da noi che abbiamo da 11 a 17 anni. Nella nostra radio, dei contenuti nessun adulto si impiccia. Non abbiamo bandiera politica ne religiosa. Facciamo e diciamo cio che vogliamo.</p>
+                    <p>Un'antenna pronta a trasmettere e ricevere i segnali del mondo che verra</p>
+			</div>
+		</header>
+
+		<!-- Content -->
+	 	<main role="main" class="container d-block">
+
+		 <!-- About us -->
+		 <div class="row py-4">
+			 <div class="d-flex">
+				 <div><h2>Chi siamo</h2></div>
+			 </div>
+		 </div>
+
+		 <!-- Radio -->
+		 <div class="row py-4">
+		 </div>
+
+		 <!-- Podcasts -->
+		 <div class="row py-4">
+		 </div>
+
+		 <!-- Videos -->
+		 <div class="row py-4">
+		 </div>
+
+		 <!-- Articles -->
+		 <div class="row py-4">
+		 </div>
+
+		 <!-- Collaborators -->
+		 <div class="row py-4">
+		 </div>
+
+		 <!-- Contact us -->
+		 <div class="row py-4">
+		 </div>
+
+		 <!-- Newsletter -->
+		 <div class="row py-4">
+		 </div>
+
+		 <!-- Events -->
+		 <div class="row py-4">
+		 </div>
+	 </main>
+
+	 <!-- Footer-->
+	 <footer class="footer fixed-bottom py-4 bg-light">
+			 <div class="container">
+
+			 </div>
+	 </footer>
+
+	<!-- Bootstrap core JavaScript -->
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+</html>
